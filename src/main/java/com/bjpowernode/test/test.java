ab35710978863db3cf33a26d314652de999a28cd
duxinlin <17877735620@qq.com>
2020-05-16 17:58:07 +0800

    添加了age

--- src/main/java/com/bjpowernode/test/test.java
+++ src/main/java/com/bjpowernode/test/test.java
@@ -11,6 +11,15 @@ package com.bjpowernode.test;
 public class test {
     private Integer id;
     private String name;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     public Integer getId() {
         return id;

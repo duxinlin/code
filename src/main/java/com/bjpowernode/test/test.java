e944640bbee6231ad3d8af6c8e29f329f4d4ad3a
duxinlin <17877735620@qq.com>
2020-05-16 17:39:47 +0800

    提交了pom和test

---
+++ src/main/java/com/bjpowernode/test/test.java
@@ -0,0 +1,30 @@
+package com.bjpowernode.test;
+
+/**
+ * ClassName:test
+ * Package:com.bjpowernode.test
+ * Description:
+ *
+ * @date:2020/5/16 17:26
+ * @author:杜欣霖
+ */
+public class test {
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
